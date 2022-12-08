4d6b79079c5cd159f8d5d4bcd28942cfe941029f
Yusuf Aji Wibowo <yusuf.wibowo@mekari.com>
2022-12-08 23:14:48 +0700

    Initial Commit

---
+++ TriangleArea/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TriangleArea
+//
+//  Created by Yusuf Aji Wibowo on 08/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
